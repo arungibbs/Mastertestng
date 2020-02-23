cb7ade6c81bf5d78bcaf490c395ab1b25e8483cb
arungibbs@gmail.com <selvakumaranbalagan281289@gmail.com>
2020-02-23 18:07:51 +0530

    successfully added 2nd time

--- src/test/java/org/india/Tng/AppTest.java
+++ src/test/java/org/india/Tng/AppTest.java
@@ -10,27 +10,29 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
+	
+	
+       public AppTest( String testName )
     {
         super( testName );
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
+        System.out.println("ARUN VIVEK VIGNESH");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
+
     public void testApp()
     {
         assertTrue( true );
